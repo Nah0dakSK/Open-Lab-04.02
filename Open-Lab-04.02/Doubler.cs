2c0380497831b32ed4922bc21cb8b3e745563b90
Patrik Fatura <patofatura@gmail.com>
2020-11-02 15:10:03 +0100

    Done

--- Open-Lab-04.02/Doubler.cs
+++ Open-Lab-04.02/Doubler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Open_Lab_04._02
 {
@@ -6,7 +7,13 @@ namespace Open_Lab_04._02
     {
         public string DoubleChar(string original)
         {
-            throw new NotImplementedException();
+            original = "abcd";            
+            original.Replace("a", "aa");
+            original.Replace("b", "bb");
+            original.Replace("c", "cc");
+            original.Replace("d", "dd");
+            return original;
         }
+       
     }
 }
